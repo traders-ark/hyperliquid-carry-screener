a7118e2fa9cbc3c50662835505f0a1f7060bfbda
traders-ark <traders-ark@users.noreply.github.com>
2024-10-25 15:04:59 +0000

    deploy: 30d43d9582c1362be50cc90e2eef88232f6543fa

--- script.js
+++ script.js
@@ -1,24 +1,41 @@
+function populateTable(tableId, dataArray, rateKey) {
+    var tableBody = $(tableId + ' tbody');
+    dataArray.forEach(function (entry) {
+        var rate = entry[rateKey];
+        var rateFormatted = rate.toFixed(6);
+        var rowClass = '';
+
+        if (rate < 0) {
+            // Negative funding rate: green (you get paid to long)
+            rowClass = 'negative-funding';
+        } else if (rate > 0) {
+            // Positive funding rate: red (you get paid to short)
+            rowClass = 'positive-funding';
+        }
+
+        tableBody.append('<tr class="' + rowClass + '"><td>' + entry.coin + '</td><td>' + rateFormatted + '</td></tr>');
+    });
+    $(tableId).DataTable({
+        paging: false,
+        searching: false
+    });
+}
+
 $(document).ready(function () {
-    // Load the JSON data
     $.getJSON('funding_data.json', function (data) {
         // Update the timestamp
         $('#timestamp').text(data.timestamp);
 
-        // Populate the tables
+        if (data.not_enough_data) {
+            $('#averageTables').html('<p>Not enough data to calculate averages.</p>');
+        } else {
+            // Populate the average tables
+            populateTable('#positiveAvgTable', data.positive_avg, 'fundingRate_percent_avg');
+            populateTable('#negativeAvgTable', data.negative_avg, 'fundingRate_percent_avg');
+        }
+
+        // Populate current funding rate tables
         populateTable('#positiveCurrentTable', data.positive_current, 'fundingRate_percent');
         populateTable('#negativeCurrentTable', data.negative_current, 'fundingRate_percent');
-        populateTable('#positiveAvgTable', data.positive_avg, 'fundingRate_percent_avg');
-        populateTable('#negativeAvgTable', data.negative_avg, 'fundingRate_percent_avg');
     });
-
-    function populateTable(tableId, dataArray, rateKey) {
-        var tableBody = $(tableId + ' tbody');
-        dataArray.forEach(function (entry) {
-            tableBody.append('<tr><td>' + entry.coin + '</td><td>' + entry[rateKey].toFixed(6) + '</td></tr>');
-        });
-        $(tableId).DataTable({
-            paging: false,
-            searching: false
-        });
-    }
 });
