3935f5726ab83cb923f067904e821dce9ca0f303
traders-ark <traders-ark@users.noreply.github.com>
2024-10-25 15:32:43 +0000

    deploy: 2408b63c28303ad8cae7578898af493a6442619a

--- script.js
+++ script.js
@@ -1,3 +1,37 @@
+function populateAvgTable(tableId, dataArray) {
+    var tableBody = $(tableId + ' tbody');
+    dataArray.forEach(function (entry) {
+        var rate = entry['fundingRate_percent_avg'];
+        var rowClass = '';
+
+        if (rate === null) {
+            // Not enough data
+            var rateDisplay = 'Not enough data';
+        } else {
+            var rateFormatted = rate.toFixed(6);
+            var rateDisplay = rateFormatted;
+
+            if (rate < 0) {
+                // Negative funding rate: green (you get paid to long)
+                rowClass = 'negative-funding';
+            } else if (rate > 0) {
+                // Positive funding rate: red (you get paid to short)
+                rowClass = 'positive-funding';
+            }
+        }
+
+        tableBody.append('<tr class="' + rowClass + '"><td>' + entry.coin + '</td><td>' + rateDisplay + '</td></tr>');
+    });
+    // Initialize DataTable if not already initialized
+    if (!$.fn.DataTable.isDataTable(tableId)) {
+        $(tableId).DataTable({
+            paging: false,
+            searching: false,
+            order: [[1, 'desc']]
+        });
+    }
+}
+
 function populateTable(tableId, dataArray, rateKey) {
     var tableBody = $(tableId + ' tbody');
     dataArray.forEach(function (entry) {
@@ -13,12 +47,18 @@ function populateTable(tableId, dataArray, rateKey) {
             rowClass = 'positive-funding';
         }
 
-        tableBody.append('<tr class="' + rowClass + '"><td>' + entry.coin + '</td><td>' + rateFormatted + '</td></tr>');
+        var rateDisplay = rateFormatted;
+
+        tableBody.append('<tr class="' + rowClass + '"><td>' + entry.coin + '</td><td>' + rateDisplay + '</td></tr>');
     });
-    $(tableId).DataTable({
-        paging: false,
-        searching: false
-    });
+    // Initialize DataTable if not already initialized
+    if (!$.fn.DataTable.isDataTable(tableId)) {
+        $(tableId).DataTable({
+            paging: false,
+            searching: false,
+            order: [[1, 'desc']]
+        });
+    }
 }
 
 $(document).ready(function () {
@@ -26,13 +66,8 @@ $(document).ready(function () {
         // Update the timestamp
         $('#timestamp').text(data.timestamp);
 
-        if (data.not_enough_data) {
-            $('#averageTables').html('<p>Not enough data to calculate averages.</p>');
-        } else {
-            // Populate the average tables
-            populateTable('#positiveAvgTable', data.positive_avg, 'fundingRate_percent_avg');
-            populateTable('#negativeAvgTable', data.negative_avg, 'fundingRate_percent_avg');
-        }
+        // Populate the average funding rates table
+        populateAvgTable('#averageFundingTable', data.avg_funding_rates);
 
         // Populate current funding rate tables
         populateTable('#positiveCurrentTable', data.positive_current, 'fundingRate_percent');
